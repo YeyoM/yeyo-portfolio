be7cadba6db18e2fff0df7d3619706d6a1ac7bf9
YeyoM <yeyomoreno2003@hotmail.com>
2024-12-29 14:46:39 -0600

    fix: :fire: Modify github action worklow for deploying infra

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -9,3 +9,7 @@ output "s3_bucket_portfolio_arn" {
 output "s3_bucket_portfolio_www_arn" {
   value = aws_s3_bucket.www_yeyom_tech.arn
 }
+
+output "s3_bucket_terraform_state_id" {
+  value = aws_s3_bucket.terraform_bucket.id
+}
